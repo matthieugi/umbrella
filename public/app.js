2ad63363728aef544ed605a0d6610f5454ce1998
matthieugi <matthieugirard@outlook.com>
2020-11-13 13:52:11 +0000

    Fix public web url

--- public/app.js
+++ public/app.js
@@ -23,7 +23,7 @@ const app = new Vue({
       if (response.ok) {
         const result = await response.json();
         this.formVisible = false;
-        this.created = `http://localhost:7071/api/${result.slug}`;
+        this.created = `${window.location.href}/api/${result.slug}`;
       } else if (response.status === 429) {
         this.error = 'You are sending too many requests. Try again in 30 seconds.';
       } else {
